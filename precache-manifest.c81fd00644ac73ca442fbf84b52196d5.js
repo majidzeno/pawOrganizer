29a3b7e0ccaaab3a472bef33fba6f504a88dcb3c
majid <majid.eltayeb@gmail.com>
2019-06-12 15:23:16 +0200

    Updates

---
+++ precache-manifest.c81fd00644ac73ca442fbf84b52196d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0814f639d88d6f69244a584c7f7c3281",
+    "url": "/pawOrganizer/index.html"
+  },
+  {
+    "revision": "820850698965e4e50103",
+    "url": "/pawOrganizer/static/css/2.6d2c8d95.chunk.css"
+  },
+  {
+    "revision": "58356ec6dea97b386a47",
+    "url": "/pawOrganizer/static/css/main.7f6c942a.chunk.css"
+  },
+  {
+    "revision": "820850698965e4e50103",
+    "url": "/pawOrganizer/static/js/2.98406a38.chunk.js"
+  },
+  {
+    "revision": "58356ec6dea97b386a47",
+    "url": "/pawOrganizer/static/js/main.05321412.chunk.js"
+  },
+  {
+    "revision": "b05fc7c49b9d735c0664",
+    "url": "/pawOrganizer/static/js/runtime~main.0baaf028.js"
+  },
+  {
+    "revision": "28fb788ee6123377f5c93164e67eebd3",
+    "url": "/pawOrganizer/static/media/Cairo-Bold.28fb788e.ttf"
+  },
+  {
+    "revision": "882f12f7383f7b4be762259cc763fb64",
+    "url": "/pawOrganizer/static/media/Cairo-Regular.882f12f7.ttf"
+  },
+  {
+    "revision": "6e318a70bb258281a4ad71c3c2ce6b57",
+    "url": "/pawOrganizer/static/media/Cairo-SemiBold.6e318a70.ttf"
+  },
+  {
+    "revision": "65578855f98d8428a4cf500ca4b44701",
+    "url": "/pawOrganizer/static/media/logo.65578855.svg"
+  }
+]);
